74a39189dc008933b6cc978915982e1c7e4a4dca
Fernando Rodrigues Jorge <fjorge@PMI.CORP>
2023-10-19 11:53:17 -0300

    Modal funcionando
    Construção do modal para o formulário de cadastro de impressoras

--- pages/home/home.js
+++ pages/home/home.js
@@ -20,3 +20,17 @@ for (let i=0; i < links.length; i++) {
         navLinks.classList.toggle('hide')
     })
 }
+
+const openModalButton = document.getElementById('open-modal');
+const closeModalButton = document.getElementById('close-modal');
+const modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+const toggleModal =() => {
+    modal.classList.toggle("hide")
+    fade.classList.toggle("hide")
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener('click', () => toggleModal())
+})
